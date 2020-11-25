629673313729f5ce65a2000756b11c57e266a611
pitust <piotr@stelmaszek.com>
2020-11-25 18:29:13 +0000

    Fixed some things.
    Signed-off-by: pitust <piotr@stelmaszek.com>

--- src/entry.rs
+++ src/entry.rs
@@ -5,6 +5,23 @@ extern {
     fn __main() -> ();
 }
 
+global_asm!("
+_start:
+    mov rsp, stack_bottom
+    mov rdi, 11 ; sys_sbrk
+    mov rsi, 0x10000 ; len
+    mov r8, 0
+    syscall
+    mov rsp, rax
+    jmp _liboh_entry
+    do_syscall sys_klog, rax, 0
+    jmp $
+align 8
+stack_top:
+    resb 4096
+stack_bottom:
+");
+
 #[macro_export]
 macro_rules! main {
     ($fn: ident) => {
@@ -13,7 +30,7 @@ macro_rules! main {
 }
 
 #[no_mangle]
-extern "C" fn _start() -> ! {
+extern "C" fn _liboh_entry() -> ! {
     crate::alloction::init();
     unsafe { __main(); }
     crate::syscall::sys_exit();
--- src/prelude.rs
+++ src/prelude.rs
@@ -4,3 +4,4 @@ pub use alloc::{
     prelude::v1::*
 };
 pub use core::sync::atomic::*;
+pub use crate::main;
